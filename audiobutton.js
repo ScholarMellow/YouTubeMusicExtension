77d1e075ecc344873a12295fc047a96951e243a4
ScholarMellow <bermudezandrew9@gmail.com>
2018-09-03 08:25:17 -0400

    first commit

---
+++ audiobutton.js
@@ -0,0 +1,23 @@
+/*var AOY_ID = "pkocpiliahoaohbolmkelakpiphnllog";
+	//AOY for Audio Only Youtube
+
+if (document.querySelectorAll("a[href='/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ']")[0].innerHTML != null) {
+	RVC = document.querySelectorAll("a[href='/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ']")[0].innerHTML
+	//RVC for Retrieved Video Catsegory
+	return RVC
+}
+
+var pageidentifier = function()  {
+	if ("RVC".equals("Music");) {
+chrome.runtime.sendMessage({message: "hi"}, function(response) {
+	alert(response.response);
+});
+	 
+		function(response, message) {
+			alert(JSON.parse(response));
+	else {
+		alert(RVC);*/
+/*		chrome.runtime.sendMessage({message:"false"}, function(response){
+			alert((message[0]));
+		});*/
+/*};*/
